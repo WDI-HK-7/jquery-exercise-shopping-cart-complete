1a40f418b9f49f604e72a3aaa36df8d4f6ea83aa
Harry Chen <harrychenca@gmail.com>
2014-12-01 07:40:33 +0800

    Fully functional shopping cart

---
+++ js/application.js
@@ -0,0 +1,73 @@
+$(document).ready(function(){
+  var returnSubtotalPrice = function(price, quantity, i){
+    var itemSubtotalPrices = $('.item-subtotal');
+    $(itemSubtotalPrices[i]).text('$'+(price*quantity).toFixed(2));
+  };
+
+  var returnTotalPrice = function(){
+    var itemPrices = $('.item-price');
+    var itemQuantities = $('.quantity');
+    var itemLength = itemPrices.length;
+    var totalPrice = 0;
+
+    for (var i=0; i < itemLength; i++){
+      var price = $(itemPrices[i]).text().replace('$','');
+      if ($.isNumeric(itemQuantities[i].value)) {
+        var quantity = itemQuantities[i].value;
+        $(itemQuantities[i]).css('color', 'black');
+      } else {
+        var quantity = 0;
+        $('#total-price').after("")
+        $(itemQuantities[i]).css('color', 'red');
+      }
+
+      returnSubtotalPrice(price, quantity, i);
+      totalPrice += price*quantity;
+    }
+
+    $('#total-price').text('$'+totalPrice.toFixed(2));
+  };
+
+  var createItem = function(){
+    // must check price is integer
+    var itemName = $('#new-item-name').val();
+    var itemUnitPrice = $('#new-item-unit-price').val();
+
+    if ($.isNumeric(itemUnitPrice) == false){
+      alert('Unit price must be a number');
+    }else if (itemName == ''){
+      alert('Item name cannot be empty');
+    }else{
+      itemUnitPrice = Number(itemUnitPrice).toFixed(2)
+      var newItem = '<div class="item row"><div class="item-name col-xs-4">'+itemName+'</div><div class="item-price col-xs-3">$'+itemUnitPrice+'</div><div class="item-qty col-xs-3"><label>QTY</label><input class="quantity" value="0"><button class="cancel">Cancel</button></div><div class="item-subtotal col-xs-2">$0.00</div></div>';
+      $('#items-list').prepend(newItem);
+    }
+  };
+
+  $('#calc-prices-button').click(function(){
+    returnTotalPrice();
+  });
+
+  // blur vs. delegate focusout
+  // $('.quantity').blur(function(){
+  $(document).delegate('.quantity', 'focusout', function(){
+    returnTotalPrice();
+  });
+
+  $('#new-item-create').click(function(){
+    createItem();
+  });
+
+  // delegate vs. bind
+  // $('.cancel').bind('click',function(){}
+  $(document).delegate('.cancel', 'click',function(){
+    $(this).parent().parent().html('');
+    returnTotalPrice();
+  });
+});
+
+// for (var name in myObject) {
+//   if (myObject.hasOwnProperty(name)) {
+//     alert(name);
+//   }
+// }
